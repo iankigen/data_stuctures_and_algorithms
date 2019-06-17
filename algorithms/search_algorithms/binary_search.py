cf371313b220426a6dbc909d74cc0e385c1d36d2
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-06-17 17:29:19 +0300

    add binary search using recursion

--- algorithms/search_algorithms/binary_search.py
+++ algorithms/search_algorithms/binary_search.py
@@ -19,22 +19,44 @@ data = [2, 7, 19, 34, 53, 72]
 
 
 def binary_search(data_list, val):
-    first = 0
-    last = len(data_list) - 1
+	first = 0
+	last = len(data_list) - 1
 
-    while first <= last:
-        midpoint = (first + last) // 2
+	while first <= last:
+		midpoint = (first + last) // 2
 
-        if val == data_list[midpoint]:
-            return True
-        else:
-            if val < data_list[midpoint]:
-                last = midpoint - 1
-            else:
-                first = midpoint + 1
-    if first > last:
-        return False
+		if val == data_list[midpoint]:
+			return True
+		else:
+			if val < data_list[midpoint]:
+				last = midpoint - 1
+			else:
+				first = midpoint + 1
+	if first > last:
+		return False
 
 
 print(binary_search(data, 34))
 print(binary_search(data, 340))
+
+
+def binary_search_recursion(sorted_list, value):
+	midpoint_index = (len(sorted_list) // 2) - 1
+	last_index = len(sorted_list) - 1
+	first_index = 0
+
+	if midpoint_index > last_index:
+		return False
+	if midpoint_index < first_index:
+		return False
+
+	if sorted_list[midpoint_index] == value:
+		return True
+	if sorted_list[midpoint_index] < value:
+		return binary_search_recursion(sorted_list[(midpoint_index + 1):], value)
+	else:
+		return binary_search_recursion(sorted_list[:midpoint_index], value)
+
+
+print(binary_search_recursion(data, 34))
+print(binary_search_recursion(data, 340))
